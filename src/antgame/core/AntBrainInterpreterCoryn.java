a404d83b1eccfd467b6b329a41a4627ada2e07d1
Doniyor Ulmasov <doniyor@live.com>
2013-04-03 01:20:08 +0100

    Minor changes to the antbrain interp
    Added statics to be able to call methods without need to create the
    object.

--- src/antgame/core/AntBrainInterpreterCoryn.java
+++ src/antgame/core/AntBrainInterpreterCoryn.java
@@ -9,16 +9,16 @@ import java.util.regex.Pattern;
 
 
 public class AntBrainInterpreterCoryn {
-	private String regSense = "sense\\s(here|ahead|leftahead|rightahead)\\s[0-9]{1,4}\\s[0-9]{1,4}\\s(friend|foe|friendwithfood|foewithfood|food|rock|marker\\s\\d|foemarker|home|foehome)(|\\s;.*)";
-	private String regMark = "mark\\s(0|1|2|3|4|5)\\s[0-9]{1,4}(|\\s;.*)";
-	private String regUnMark = "unmark\\s(0|1|2|3|4|5)\\s[0-9]{1,4}(|\\s;.*)";
-	private String regPickUp = "pickup\\s[0-9]{1,4}\\s[0-9]{1,4}(|\\s;.*)";
-	private	String regDrop = "drop\\s[0-9]{1,4}(|\\s;.*)";
-	private	String regTurn = "turn\\s(left|right)\\s[0-9]{1,4}(|\\s;.*)";
-	private	String regMove = "move\\s[0-9]{1,4}\\s[0-9]{1,4}(|\\s;.*)";
-	private	String regFlip = "flip\\s[0-9]{1,4}\\s[0-9]{1,4}\\s[0-9]{1,4}(|\\s;.*)";
-	private ArrayList<String> regExpressions = new ArrayList<String>();
-	
+	private final static String regSense = "sense\\s(here|ahead|leftahead|rightahead)\\s[0-9]{1,4}\\s[0-9]{1,4}\\s(friend|foe|friendwithfood|foewithfood|food|rock|marker\\s\\d|foemarker|home|foehome)(|\\s;.*)";
+	private final static String regMark = "mark\\s(0|1|2|3|4|5)\\s[0-9]{1,4}(|\\s;.*)";
+	private final static String regUnMark = "unmark\\s(0|1|2|3|4|5)\\s[0-9]{1,4}(|\\s;.*)";
+	private final static String regPickUp = "pickup\\s[0-9]{1,4}\\s[0-9]{1,4}(|\\s;.*)";
+	private final static String regDrop = "drop\\s[0-9]{1,4}(|\\s;.*)";
+	private final static String regTurn = "turn\\s(left|right)\\s[0-9]{1,4}(|\\s;.*)";
+	private final static String regMove = "move\\s[0-9]{1,4}\\s[0-9]{1,4}(|\\s;.*)";
+	private final static String regFlip = "flip\\s[0-9]{1,4}\\s[0-9]{1,4}\\s[0-9]{1,4}(|\\s;.*)";
+	private static ArrayList<String> regExpressions = new ArrayList<String>();
+												
 	private Pattern pattSense = Pattern.compile("sense\\s(here|ahead|leftahead|rightahead)\\s([0-9]{1,4})\\s([0-9]{1,4})\\s(friend|foe|friendwithfood|foewithfood|food|rock|marker\\s\\d|foemarker|home|foehome)(|\\s;.*)");
 	private Pattern pattMark = Pattern.compile("mark\\s(0|1|2|3|4|5)\\s([0-9]{1,4})(|\\s;.*)");
 	private Pattern pattUnMark = Pattern.compile("unmark\\s(0|1|2|3|4|5)\\s([0-9]{1,4})(|\\s;.*)");
@@ -48,164 +48,160 @@ public class AntBrainInterpreterCoryn {
 	public BrainState[] antBrainGenerator(String antBrainTextFile, AntColour colour){
 		if(antBrainChecker(antBrainTextFile)){
 			BrainState[] states = new BrainState[stateInt];
-			
-		
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(antBrainTextFile));
-			int ptr = 0;
-			String curLineup = reader.readLine();
-			while(curLineup != null){
-				String curLine = curLineup.toLowerCase();
-				String instruction; 
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(antBrainTextFile));
+				int ptr = 0;
+				String curLineup = reader.readLine();
+				while(curLineup != null){
+					String curLine = curLineup.toLowerCase();
+					String instruction; 
+									
+					if(curLine.matches(regSense)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.SENSE);
+						bs.setStateId(ptr);
 				
-				
-				
-				if(curLine.matches(regSense)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.SENSE);
-					bs.setStateId(ptr);
-			
-					Matcher mattSense = pattSense.matcher(curLine);
-					if (mattSense.find())
-					{
-						bs.setSenseDirection(mattSense.group(1));
-						bs.setNextIdState(Integer.parseInt(mattSense.group(2)));
-					    bs.setAltNextIdState(Integer.parseInt(mattSense.group(3)));
-					    if(mattSense.group(4).startsWith("marker")){
-					    	bs.setSenseCondition("marker");
-					    	bs.setMarker(Integer.parseInt(mattSense.group(4).substring(7, 8)),colour);
-					    }
-					    else{
-					    	bs.setSenseCondition(mattSense.group(4));
-					    }
-					}
-					states[ptr] = bs;
-					
-				}
-				else if(curLine.matches(regMark)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.MARK);
-					bs.setStateId(ptr);
-			
-					Matcher mattMark = pattMark.matcher(curLine);
-					if (mattMark.find())
-					{
-						bs.setMarker(Integer.parseInt(mattMark.group(1)),colour);
-						bs.setNextIdState(Integer.parseInt(mattMark.group(2)));
-					}
-					states[ptr] = bs;
-					
-				}
-				else if(curLine.matches(regUnMark)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.UNMARK);
-					bs.setStateId(ptr);
-			
-					Matcher mattUnMark = pattUnMark.matcher(curLine);
-					if (mattUnMark.find())
-					{
-						bs.setMarker(Integer.parseInt(mattUnMark.group(1)),colour);
-						bs.setNextIdState(Integer.parseInt(mattUnMark.group(2)));
-					}
-					states[ptr] = bs;
-					
-				}
-				else if(curLine.matches(regPickUp)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.PICKUP);
-					bs.setStateId(ptr);
-			
-					Matcher mattPickUp = pattPickUp.matcher(curLine);
-					if (mattPickUp.find())
-					{
-						bs.setNextIdState(Integer.parseInt(mattPickUp.group(1)));
-						bs.setAltNextIdState(Integer.parseInt(mattPickUp.group(2)));
-					}
-					states[ptr] = bs;
-					
-					
-				}
-				else if(curLine.matches(regDrop)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.DROP);
-					bs.setStateId(ptr);
-			
-					Matcher mattDrop = pattDrop.matcher(curLine);
-					if (mattDrop.find())
-					{
-						bs.setNextIdState(Integer.parseInt(mattDrop.group(1)));
-					}
-					states[ptr] = bs;
-					
-					
-				}
-				else if(curLine.matches(regTurn)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.TURN);
-					bs.setStateId(ptr);
-			
-					Matcher mattTurn = pattTurn.matcher(curLine);
-					if (mattTurn.find())
-					{
+						Matcher mattSense = pattSense.matcher(curLine);
+						if (mattSense.find())
+						{
+							bs.setSenseDirection(mattSense.group(1));
+							bs.setNextIdState(Integer.parseInt(mattSense.group(2)));
+						    bs.setAltNextIdState(Integer.parseInt(mattSense.group(3)));
+						    if(mattSense.group(4).startsWith("marker")){
+						    	bs.setSenseCondition("marker");
+						    	bs.setMarker(Integer.parseInt(mattSense.group(4).substring(7, 8)),colour);
+						    }
+						    else{
+						    	bs.setSenseCondition(mattSense.group(4));
+						    }
+						}
+						states[ptr] = bs;
 						
-						bs.setLeftRight(mattTurn.group(1));
-						bs.setNextIdState(Integer.parseInt(mattTurn.group(2)));
 					}
-					states[ptr] = bs;
-					
-					
-				}
-				else if(curLine.matches(regMove)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.MOVE);
-					bs.setStateId(ptr);
-			
-					Matcher mattMove = pattMove.matcher(curLine);
-					if (mattMove.find())
-					{
+					else if(curLine.matches(regMark)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.MARK);
+						bs.setStateId(ptr);
+				
+						Matcher mattMark = pattMark.matcher(curLine);
+						if (mattMark.find())
+						{
+							bs.setMarker(Integer.parseInt(mattMark.group(1)),colour);
+							bs.setNextIdState(Integer.parseInt(mattMark.group(2)));
+						}
+						states[ptr] = bs;
 						
-						bs.setNextIdState(Integer.parseInt(mattMove.group(1)));
-						bs.setAltNextIdState(Integer.parseInt(mattMove.group(2)));
 					}
-					states[ptr] = bs;
-					
-					
-				}
-				else if(curLine.matches(regFlip)){
-					BrainState bs =  new BrainState();
-					bs.setInstruction(Instruction.FLIP);
-					bs.setStateId(ptr);
-			
-					Matcher mattFlip = pattFlip.matcher(curLine);
-					if (mattFlip.find())
-					{
-						bs.setRandomInt(Integer.parseInt(mattFlip.group(1)));
-						bs.setNextIdState(Integer.parseInt(mattFlip.group(2)));
-						bs.setAltNextIdState(Integer.parseInt(mattFlip.group(3)));
-					}
-					states[ptr] = bs;
-					
-					
-				}
+					else if(curLine.matches(regUnMark)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.UNMARK);
+						bs.setStateId(ptr);
 				
-				
-				ptr++;
-				
+						Matcher mattUnMark = pattUnMark.matcher(curLine);
+						if (mattUnMark.find())
+						{
+							bs.setMarker(Integer.parseInt(mattUnMark.group(1)),colour);
+							bs.setNextIdState(Integer.parseInt(mattUnMark.group(2)));
+						}
+						states[ptr] = bs;
 						
+					}
+					else if(curLine.matches(regPickUp)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.PICKUP);
+						bs.setStateId(ptr);
 				
-				curLineup = reader.readLine();
+						Matcher mattPickUp = pattPickUp.matcher(curLine);
+						if (mattPickUp.find())
+						{
+							bs.setNextIdState(Integer.parseInt(mattPickUp.group(1)));
+							bs.setAltNextIdState(Integer.parseInt(mattPickUp.group(2)));
+						}
+						states[ptr] = bs;
+						
+						
+					}
+					else if(curLine.matches(regDrop)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.DROP);
+						bs.setStateId(ptr);
+				
+						Matcher mattDrop = pattDrop.matcher(curLine);
+						if (mattDrop.find())
+						{
+							bs.setNextIdState(Integer.parseInt(mattDrop.group(1)));
+						}
+						states[ptr] = bs;
+						
+						
+					}
+					else if(curLine.matches(regTurn)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.TURN);
+						bs.setStateId(ptr);
+				
+						Matcher mattTurn = pattTurn.matcher(curLine);
+						if (mattTurn.find())
+						{
+							
+							bs.setLeftRight(mattTurn.group(1));
+							bs.setNextIdState(Integer.parseInt(mattTurn.group(2)));
+						}
+						states[ptr] = bs;
+						
+						
+					}
+					else if(curLine.matches(regMove)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.MOVE);
+						bs.setStateId(ptr);
+				
+						Matcher mattMove = pattMove.matcher(curLine);
+						if (mattMove.find())
+						{
+							
+							bs.setNextIdState(Integer.parseInt(mattMove.group(1)));
+							bs.setAltNextIdState(Integer.parseInt(mattMove.group(2)));
+						}
+						states[ptr] = bs;
+						
+						
+					}
+					else if(curLine.matches(regFlip)){
+						BrainState bs =  new BrainState();
+						bs.setInstruction(Instruction.FLIP);
+						bs.setStateId(ptr);
+				
+						Matcher mattFlip = pattFlip.matcher(curLine);
+						if (mattFlip.find())
+						{
+							bs.setRandomInt(Integer.parseInt(mattFlip.group(1)));
+							bs.setNextIdState(Integer.parseInt(mattFlip.group(2)));
+							bs.setAltNextIdState(Integer.parseInt(mattFlip.group(3)));
+						}
+						states[ptr] = bs;
+						
+						
+					}
+					
+					
+					ptr++;
+					
+							
+					
+					curLineup = reader.readLine();
+				}
+			
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		updateBrainStates(states);
-		return states;	
-		
+			updateBrainStates(states);
+			return states;	
+			
 		}
 		else{
 			
@@ -213,7 +209,7 @@ public class AntBrainInterpreterCoryn {
 			return null;
 		}
 	}
-	public boolean antBrainChecker(String antBrainTextFile){
+	public static boolean antBrainChecker(String antBrainTextFile){
 		boolean legit = false;
 		int numStates = 0;
 		try {
@@ -266,7 +262,7 @@ public class AntBrainInterpreterCoryn {
 		}
 	}
 	
-	public boolean regChecker(String s){
+	public static boolean regChecker(String s){
 		for(String reg: regExpressions){
 			
 			String sLower = s.toLowerCase();
