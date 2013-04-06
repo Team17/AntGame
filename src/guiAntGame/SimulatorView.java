f0c35cc0f280bc156e738e80c93a356c917679bb
Doniyor Ulmasov <doniyor@live.com>
2013-04-07 00:58:02 +0100

    Updated icons

--- src/guiAntGame/SimulatorView.java
+++ src/guiAntGame/SimulatorView.java
@@ -1,11 +1,10 @@
 package guiAntGame;
 
-import java.util.ArrayList;
-
 import antgame.core.Cell;
 import antgame.core.Map;
 import antgame.core.World;
 import processing.core.*;
+import org.gicentre.utils.move.*;
 
 public class SimulatorView extends PApplet {
 	private Hexagon[][] board;
@@ -14,6 +13,7 @@ public class SimulatorView extends PApplet {
 //	private Observer obiwan;
 	private Map curMap;
 	private World w;
+	ZoomPan zoomer;
 	
 //	public void setup(Observer obiwan,int xSize, int ySize) {
 	public void setup() {
@@ -23,13 +23,15 @@ public class SimulatorView extends PApplet {
 		World w1 = new World("C:/workingworld.world"/*workingDir+"\\files\\workingworld.world"**/,workingDir+"\\files\\cleverbrain1.brain",workingDir+"\\files\\cleverbrain2.brain");
 
 		size(800, 800);
-		background(0);
-		this.xSize=150;
-		this.ySize=150;
+		frameRate(2000);
+		zoomer = new ZoomPan(this);
+		zoomer.allowZoomButton(false);
+
 //		this.obiwan = obiwan;
 		this.w = w1;
 		curMap = w.getMap();
-		
+		this.xSize=curMap.getXSize();
+		this.ySize=curMap.getYSize();
 		rock = loadImage("rock.png");
 		food = loadImage("food.png");
 		antHR = loadImage("redH.png");
@@ -46,37 +48,40 @@ public class SimulatorView extends PApplet {
 					if(cCell.containsRock()){
 						board[x][y] = new Hexagon(this, rock);
 					}
-					else if(cCell.isClear()){
-						board[x][y] = new Hexagon(this, clear);
-					}
+
 					else if(cCell.containsBlackAnt()){
 						board[x][y] = new Hexagon(this, antB);
 					}
 					else if(cCell.containsRedAnt()){
 						board[x][y] = new Hexagon(this, antR);
 					}
+					else if(cCell.isContainsFood()){
+						board[x][y] = new Hexagon(this, food);
+					}	
 					else if(cCell.containsBlackAntHill()){
 						board[x][y] = new Hexagon(this, antHB);
 					}
 					else if(cCell.containsRedAntHill()){
 						board[x][y] = new Hexagon(this, antHR);
 					}
-					else if(cCell.isContainsFood()){
-						board[x][y] = new Hexagon(this, food);
-					}					
+					else if(cCell.isClear()){
+						board[x][y] = new Hexagon(this, clear);
+					}
+				
 			}
 		}
 	}
 
 	public void draw() {
+		
+	background(70);
+	zoomer.transform();
 		for(int x=0; x<xSize;x++){
 			for(int y=0;y<ySize;y++){
-				board[x][y].display(x, y, 40, 47);	
-						
 				if(x%2==0){
-					board[x][y].display(40*y, x*35, 40, 47);
+					board[x][y].display(45*y, x*40, 40, 47);
 				}else{
-					board[x][y].display(40*y+20, x*35, 40, 47);
+					board[x][y].display(45*y+25, x*40, 40, 47);
 				}
 			}
 		}
@@ -92,23 +97,24 @@ public class SimulatorView extends PApplet {
 					if(cCell.containsRock()){
 						board[x][y] = new Hexagon(this, rock);
 					}
-					else if(cCell.isClear()){
-						board[x][y] = new Hexagon(this, clear);
-					}
+	
 					else if(cCell.containsBlackAnt()){
 						board[x][y] = new Hexagon(this, antB);
 					}
 					else if(cCell.containsRedAnt()){
 						board[x][y] = new Hexagon(this, antR);
 					}
+					else if(cCell.isContainsFood()){
+						board[x][y] = new Hexagon(this, food);
+					}	
 					else if(cCell.containsBlackAntHill()){
 						board[x][y] = new Hexagon(this, antHB);
 					}
 					else if(cCell.containsRedAntHill()){
 						board[x][y] = new Hexagon(this, antHR);
 					}
-					else if(cCell.isContainsFood()){
-						board[x][y] = new Hexagon(this, food);
+					else if(cCell.isClear()){
+						board[x][y] = new Hexagon(this, clear);
 					}					
 			}
 		}
