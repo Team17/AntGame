28ba1d087bcb45f0a8f1fc778ca7030bb20b0d8e
Doniyor Ulmasov <doniyor@live.com>
2013-04-08 22:59:18 +0100

    added fonts

--- src/guiAntGame/MainMenu.java
+++ src/guiAntGame/MainMenu.java
@@ -14,27 +14,29 @@ public class MainMenu extends PApplet {
 
 	private ControlP5 cp5;
 	private PImage draft;
+	
 
 	// public void setup(Observer obiwan,int xSize, int ySize) {
 	public void setup() {
 
 
 		size(600, 700);
-
+		PFont gothic = createFont("GOTHIC.TTF",34);
 		cp5 = new ControlP5(this);
 		cp5.addFrameRate().setInterval(10).setPosition(0, height - 10);
 
 		cp5.addTextlabel("mainText")
-				.setPosition(width/2-80,20 )
-				.setText("MAIN MENU")
-				.setFont(createFont("Calibri", 30))
-				.setColor(255);
+				.setPosition(width/2-80,30 )
+				.setText("Main Menu")
+				.setFont(gothic)
+				.setColor(240);
 		
-
+		
 		cp5.addButton("help").setPosition(50, 50).setSize(150, 20);
 
 		cp5.addButton("endgame").setPosition(500, 670).setSize(60, 20);
-		draft = loadImage("draft.jpg");
+		draft = loadImage("draftUI.jpg");
+
 		
 		
 
@@ -42,6 +44,7 @@ public class MainMenu extends PApplet {
 
 	public void draw() {
 		image(draft,0,0);
+		
 		
 	}
 
