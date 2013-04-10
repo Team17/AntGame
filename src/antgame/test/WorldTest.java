784570dd485ad143fe04c8541e2d9f33beb9abff
Cyrus Shamsfard <Cyrus@10.0.12.112>
2013-04-10 15:06:01 +0100

    Created WorldTest class

---
+++ src/antgame/test/WorldTest.java
@@ -0,0 +1,99 @@
+package antgame.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WorldTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testWorld() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSensedCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAntCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAntIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAntAtCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAntAtIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKillAnt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAntSurronded() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetObserver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWhoWon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
