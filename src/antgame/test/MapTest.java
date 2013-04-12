65249b1967e3ab68e9291774bafb3a4b39ee7b5d
cyrus soroush shamsfard <css27@hotmail.co.uk>
2013-04-12 15:56:48 +0100

    Test class

---
+++ src/antgame/test/MapTest.java
@@ -0,0 +1,74 @@
+package antgame.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MapTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintmap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCellIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCellIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetXSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetYSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacentCell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSurrondingCells() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
