e54936e77374dfc295006248add0f88106414f60
cyrus soroush shamsfard <css27@hotmail.co.uk>
2013-04-18 15:17:09 +0100

    Latest version

--- src/antgame/test/MapCreatorTest.java
+++ src/antgame/test/MapCreatorTest.java
@@ -4,55 +4,101 @@ import static org.junit.Assert.*;
 
 import java.awt.Point;
 import java.util.HashSet;
+import java.util.Iterator;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import antgame.core.Cell;
-import antgame.core.Map;
 import antgame.core.MapCreator;
 
 public class MapCreatorTest {
-	
+
+	MapCreator mc = new MapCreator();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
 	public void testGetRandomMap() {
-		assertNotNull(MapCreator.getRandomMap());
+		assertNotNull(mc.getRandomMap());
 	}
 
 	@Test
 	public void testRandomPoint() {
-		assertNotNull(MapCreator.randomPoint(45, 23));
+		assertTrue(mc.randomPoint(150, 150).getX()<=150);
+		assertTrue(mc.randomPoint(150, 150).getY()<=150);
 	}
 
 	@Test
 	public void testCanDrawElementHere() {
-		assertTrue(MapCreator.canDrawElementHere(new Map(new Cell[2][2]), new HashSet<Point>(), 2, 1));
+		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testInvertCells() {
-		Cell[][] temp = new Cell[2][2];
-		temp[0][0] = new Cell(2,3);
-		temp[0][1] = new Cell(3,4);
-		temp[1][0] = new Cell(0,8);
-		temp[1][1] = new Cell(5,6);
+		Cell[][] cell = new Cell[5][5];
+
+		for (int i = 0; i<cell.length; i++){
+			for (int j = 0; j<cell.length; j++){
+				cell[i][j] = new Cell(i,j);
+			}
+		}
+		cell[1][3].setRock();
+		cell[3][3].setRock();
+		cell[3][4].setRock(); 
+		cell[0][2].setRock();
 		
-		Cell[][] temp2 = new Cell[2][2];
-		temp[0][0] = temp[0][0];
-		temp[0][1] = temp[1][0];
-		temp[1][0] = temp[0][1];
-		temp[1][1] = temp[1][1];
 		
-		assertArrayEquals(temp2, MapCreator.invertCells(temp));
+		Cell[][] invCells = mc.invertCells(cell);
+		
+
+		assertTrue(invCells[3][1].containsRock());
+		assertTrue(invCells[3][3].containsRock());
+		assertTrue(invCells[4][3].containsRock());
+		assertTrue(invCells[2][0].containsRock());
 	}
 
 	@Test
 	public void testGetAnthillPoints() {
-		assertNotNull(MapCreator.getAnthillPoints());
+		
+		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetFoodBlobPoints() {
-		assertNotNull(MapCreator.getFoodBlobPoints());
+		
+		
+		HashSet<Point> targetCells = mc.getFoodBlobPoints();
+		
+		Iterator<Point> it = targetCells.iterator();
+		while (it.hasNext()) {
+			Point p = (Point) it.next();
+			System.out.println("fdgdsfhhh");
+			assertEquals(false , mc.getRandomMap().getCell((int)p.getX(),(int)p.getY()).isContainsFood());
+			
+		}
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
 	}
 
 }
